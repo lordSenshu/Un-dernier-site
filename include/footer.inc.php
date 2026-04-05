4e81d8abcef79f4313cb95be1c6a659e097c8515
agnies <agnies.sadli@se.univ-bejaia.dz>
2026-04-05 13:59:29 +0200

    modifier footer

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -4,19 +4,13 @@
 <footer>
     <section class="links">
         <h4>Liens utiles :</h4>
-        <a href="index.php">Index</a>
         <a href="sitemap.php">Sitemap</a>
-        <a href="ecologie.php">Ecologie</a>
         <a href="tech.php">API GHIBLI</a>
     </section>
-    <section>
-        <img src="images/logo_cy.png"/>
-        <address>33 Boulevard du Port 95011 Cergy-Pontoise</address>
-    </section>
     <section class="host">
-        <p>Hebergé chez : <a href="https://arbusmegacorp.top">Arbus MegaCorporation</a>.</p>
+        <p>EcoPlein — Trouvez le carburant le moins cher près de chez vous.</p>
+        <p>Hébergé chez <a href="https://arbusmegacorp.top">Arbus MegaCorporation</a></p>
     </section>
-
 </footer>
 </body>
 </html>
