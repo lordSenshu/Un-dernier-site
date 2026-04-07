84c1c4ca279d482396662d917489e78768ca4c36
agnies <agnies.sadli@se.univ-bejaia.dz>
2026-04-07 10:12:36 +0200

    tkt

--- sitemap.php
+++ sitemap.php
@@ -1,7 +1,7 @@
 <?php
     require "include/functions.inc.php";
     $title = "Sitemap";
-    $style = "/style/style.css";
+    $style = "/style/newcss.css";
     require_once "include/header.inc.php";
 ?>
 <h1>Voici la liste complète de toutes les pages de ce site web.</h1>
